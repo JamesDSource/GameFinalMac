1436fe287d5382c5c631dd716679fd8b75c101de
James Duran <jamesevora367@gmail.com>
2019-05-21 12:04:46 -0700

    Commit_2

--- Orb.cs
+++ Orb.cs
@@ -1,16 +1,34 @@
+
 using Godot;
 using System;
 
 public class Orb : KinematicBody2D
 {
-    public override void _Ready()
-    {}
-	public override void _PhysicsProcess()
+    private bool picked_up = false; 
+	public override void _Ready()
+    {
+		GetNode<Sprite>("PickUp").Hide();
+	}
+	public override void _PhysicsProcess(float delta)
 	{
 		
 	}
-	public override void _Process()
+	public override void _Process(float delta)
 	{
 		
+	}
+	
+	
+	private void _on_Detection_body_entered(object body)
+	{
+	    if(picked_up == false && body.ToString() == "player")
+		{GetNode<Sprite>("PickUp").Show();}
+	}
+	
+	
+	private void _on_Detection_body_exited(object body)
+	{
+	    if(picked_up = false && body.ToString() == "player")
+		{GetNode<Sprite>("PickUp").Hide();}
 	}
 }
